ab603f2b7dfc6fcc862bbe919774f4e1e2c0b5c9
Franklin Kong <franklinkong981@gmail.com>
2024-05-05 13:57:45 -0700

    Implemented methods and routes for registering and logging in a user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, redirect, session, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db 
-# from forms import 
+from models import db, connect_db, User
+from forms import RegisterUserForm, LoginUserForm
 from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
@@ -21,13 +21,53 @@ def home():
     """The homepage. Redirects to the login page."""
     return redirect("/login")
 
-@app.route("/login")
-def show_login_form():
-    """Shows form that when submitted will login a user. This form accepts a username and password. Also contains a link to the register page."""
-    return render_template("login.html")
-
-@app.route("/register")
-def show_registration_form():
+@app.route("/register", methods=['GET', 'POST'])
+def show_register_form():
     """Shows the form that when submitted will register/create a new user. This form accepts a username, password email, first name,
-    and last name."""
-    return render_template("register.html")
+    and last name. If POST, attempts to register the user by adding the enw user to the database, then redirects user to secret page."""
+    registerForm = RegisterUserForm()
+
+    if registerForm.validate_on_submit():
+        username = registerForm.username.data
+        password = registerForm.password.data
+        email = registerForm.email.data
+        first_name = registerForm.first_name.data
+        last_name = registerForm.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+        if new_user:
+            db.session.add(new_user)
+            db.session.commit()
+            session['current_user'] = new_user.username
+            flash("Welcome! Successfully created account")
+            return redirect("/secret")
+        else:
+            registerForm.username.errors.append('Username already taken. Please pick another')
+
+    return render_template("register.html", form=registerForm)
+
+@app.route("/login", methods=['GET', 'POST'])
+def show_login_form():
+    """Shows form that when submitted will login a user. This form accepts a username and password. Also contains a link to the register page.
+    If POST, attemps to log in the user. If username is found in database and hashed password matches, redirects user to secret page."""
+    loginForm = LoginUserForm()
+
+    if loginForm.validate_on_submit():
+        username = loginForm.username.data
+        password = loginForm.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}!")
+            session['current_user'] = user.username
+            return redirect('/secret')
+        else:
+            loginForm.username.errors = ['Invalid username/password']
+    
+    return render_template("login.html", form=loginForm)
+
+@app.route("/secret")
+def show_secret():
+    return render_template("secret.html")
+
+
--- models.py
+++ models.py
@@ -6,7 +6,6 @@ bcrypt = Bcrypt()
 
 def connect_db(app):
     """Connect to a database."""
-
     db.app = app
     db.init_app(app)
 
@@ -20,3 +19,28 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register user with hashed password and return user. If the username is already taken, return false."""
+        u = User.query.filter_by(username=username).first()
+        if u:
+            return False
+
+        hashed = bcrypt.generate_password_hash(password)
+        # turn bytestring into normal (unicode utf8) string
+        hashed_utf8 = hashed.decode("utf8")
+
+        # return instance of user with username and hashed password
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists and password is correct. Return user if valid, else return False."""
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, password):
+            # return user instance
+            return u
+        else:
+            return False
