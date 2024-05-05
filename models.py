78055880cd188c56a262c112e34cca0ceee0d92a
Franklin Kong <franklinkong981@gmail.com>
2024-05-05 11:43:25 -0700

    Created User model and made base template

--- models.py
+++ models.py
@@ -10,3 +10,13 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class User(db.Model):
+    """The users that have an account on Flask Feedback. Each user will have a username, a password, email, first_name, and last_name."""
+
+    __tablename__= "users"
+
+    username = db.Column(db.String(20), primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
